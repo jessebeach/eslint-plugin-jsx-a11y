091952303459e5bc4ea594d116f98e43dea262bb
Ethan Cohen <ethanvcohen@gmail.com>
2016-10-12 14:37:31 -0400

    Apply global settings to anchor-has-content rule.

--- __tests__/src/rules/anchor-has-content-test.js
+++ __tests__/src/rules/anchor-has-content-test.js
@@ -28,6 +28,10 @@ const expectedError = {
   type: 'JSXOpeningElement',
 };
 
+const options = [{
+  components: ['Route'],
+}];
+
 ruleTester.run('anchor-has-content', rule, {
   valid: [
     { code: '<div />;', parserOptions },
@@ -37,10 +41,38 @@ ruleTester.run('anchor-has-content', rule, {
     { code: '<a>{foo.bar}</a>', parserOptions },
     { code: '<a dangerouslySetInnerHTML={{ __html: "foo" }} />', parserOptions },
     { code: '<a children={children} />', parserOptions },
+
+    // OPTIONS TESTS
+    { code: '<div />;', parserOptions, options },
+    { code: '<a>Foo</a>', parserOptions, options },
+    { code: '<Route><Bar /></Route>', parserOptions, options },
+    { code: '<Route>{foo}</Route>', parserOptions, options },
+    { code: '<Route>{foo.bar}</Route>', parserOptions, options },
+    { code: '<Route dangerouslySetInnerHTML={{ __html: "foo" }} />', parserOptions, options },
+    { code: '<Route children={children} />', parserOptions, options },
+
+    // GLOBAL SETTINGS TESTS
+    { code: '<div />;', parserOptions },
+    { code: '<Link>Foo</Link>', parserOptions },
+    { code: '<Link><Bar /></Link>', parserOptions },
+    { code: '<Link>{foo}</Link>', parserOptions },
+    { code: '<Link>{foo.bar}</Link>', parserOptions },
+    { code: '<Link dangerouslySetInnerHTML={{ __html: "foo" }} />', parserOptions },
+    { code: '<Link children={children} />', parserOptions },
   ],
   invalid: [
     { code: '<a />', errors: [expectedError], parserOptions },
     { code: '<a><Bar aria-hidden /></a>', errors: [expectedError], parserOptions },
     { code: '<a>{undefined}</a>', errors: [expectedError], parserOptions },
+
+    // OPTIONS TESTS
+    { code: '<Route />', errors: [expectedError], parserOptions, options },
+    { code: '<Route><Bar aria-hidden /></Route>', errors: [expectedError], parserOptions, options },
+    { code: '<Route>{undefined}</Route>', errors: [expectedError], parserOptions, options },
+
+    // GLOBAL SETTINGS TESTS
+    { code: '<Link />', errors: [expectedError], parserOptions },
+    { code: '<Link><Bar aria-hidden /></Link>', errors: [expectedError], parserOptions },
+    { code: '<Link>{undefined}</Link>', errors: [expectedError], parserOptions },
   ],
 });
--- src/rules/anchor-has-content.js
+++ src/rules/anchor-has-content.js
@@ -21,11 +21,22 @@
  const errorMessage =
     'Anchors must have content and the content must be accessible by a screen reader.';
 
+// Rule options override global settings if set.
+ function resolveElementsToCheck(context = {}) {
+   // Global settings
+   const globalSettings = context.settings || {};
+   const componentGlobalOptions = globalSettings.components && globalSettings.components.a;
+   // Rule options
+   const ruleOptions = context.options[0] || {};
+   const componentRuleOptions = ruleOptions.components;
+
+   const componentOptions = componentRuleOptions || componentGlobalOptions;
+   return ['a'].concat(componentOptions);
+ }
+
  const rule = context => ({
    JSXOpeningElement: (node) => {
-     const options = context.options[0] || {};
-     const componentOptions = options.components || [];
-     const typeCheck = ['a'].concat(componentOptions);
+     const typeCheck = resolveElementsToCheck(context);
      const nodeType = elementType(node);
 
     // Only check anchor elements and custom types.
